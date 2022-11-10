8629af07a244c30d25dfaf92ba509e6f1c84d97a
Aniket Pathak <aniketpathak028@gmail.com>
2022-11-10 12:32:22 +0530

    add Lab11

--- Lab-10/q2.c
+++ Lab-10/q2.c
@@ -1,9 +1,6 @@
-/* A naive recursive implementation that simply
-follows the above optimal substructure property */
 #include <limits.h>
 #include <stdio.h>
 
-// Matrix Ai has dimension p[i-1] x p[i] for i = 1..n
 int MatrixChainOrder(int p[], int i, int j)
 {
     if (i == j)
@@ -20,12 +17,9 @@ int MatrixChainOrder(int p[], int i, int j)
         if (count < min)
             min = count;
     }
-
-    // Return minimum count
     return min;
 }
 
-// Driver program to test above function
 int main()
 {
     int arr[] = {1, 2, 3, 4, 3};
